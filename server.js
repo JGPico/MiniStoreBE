85416afd678bd81f3a60475ff02f2422efb689fe
JGPico <minq_2@hotmail.com>
2020-09-14 13:55:48 -0600

    ejs boiler

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+
 const helmet = require('helmet');
 const cors = require('cors');
 
 const server = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index.js');
 
 server.use(helmet());
 server.use(cors());
-server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.json({ Message: "The API is running!" });
-});
+server.set('view engine', 'ejs');
+server.set('views', __dirname + '/views');
+server.set('layout', 'layouts/layout');
+server.use(expressLayouts);
+server.use(express.static('public'));
+
+server.use('/', indexRouter);
 
 module.exports = server;
